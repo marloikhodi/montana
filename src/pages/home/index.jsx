6510fb3b0492430086d611d8122fca3573b90cea
MarloIkhodi <deivid.serafim03@gmail.com>
2024-12-04 21:43:28 -0300

    wip

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,8 +4,8 @@ import styles from "./home.module.css"
 export default function Home() {
     return (
         <section className={styles.homeSection}>
-
             <Header page="home" />
+
         </section>
     )
 }
