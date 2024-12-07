c3e2c6694cf973ad94689591c5152b5b90b60ab0
marloikhodi <deivid.serafim03@gmail.com>
2024-12-07 00:15:39 -0300

    correção na branchname

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -5,6 +5,7 @@ export default function Hero() {
     return (
         <section className={styles.heroContainer}>
             <Header page="home" />
+            {/* correção */}
         </section>
     )
 }
