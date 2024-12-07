4dabefb19195c60ffc5cb284a04886f2a6b67739
MarloIkhodi <deivid.serafim03@gmail.com>
2024-12-07 09:23:46 -0300

    completed hero section

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -5,7 +5,14 @@ export default function Hero() {
     return (
         <section className={styles.heroContainer}>
             <Header page="home" />
-            {/* correção */}
+            <div className={styles.heroTitle}>
+                <h3>Discover</h3>
+                <h1>Montana</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua. Ut enim ad minim </p>
+                <button><p>Explore Now</p></button>
+            </div>
         </section>
     )
 }
