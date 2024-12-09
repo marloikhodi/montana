6ffde257f54ddc14c3708491b48b54b6ca7c661d
marloikhodi <deivid.serafim03@gmail.com>
2024-12-08 23:27:08 -0300

    gallery images section complete

--- src/pages/gallery/index.jsx
+++ src/pages/gallery/index.jsx
@@ -1,10 +1,12 @@
-import { GalleryHeader } from "../../components"
+import { Footer, GalleryHeader, GalleryImages } from "../../components"
 import styles from "./gallery.module.css"
 
 export default function Home() {
     return (
         <section className={styles.galleryContainer}>
             <GalleryHeader />
+            <GalleryImages />
+            <Footer />
         </section>
     )
 }
